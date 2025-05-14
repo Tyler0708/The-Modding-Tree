91f4b3383dd063b1887eafc885feb09f18c310e8
Tyler0708 <triesgo11@gmail.com>
2025-05-14 18:55:37 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Cultivation Tree",
+	author: "Reshiram<3",
+	pointsName: "Qi",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
